4db8c1372c20c692c5c7d2450f489f724a2ba9e1
RonjaAlberte <ronjaash@gmail.com>
2026-03-04 16:01:39 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+col_1, col_2 = st.columns([1,10])
+
+with col_1:
+    st.markdown("Hi!")
+
+with col_2:
+    st.title("Hello??")
